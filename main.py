4094358dc4ba20137573555f08b3f6e52b043a97
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 13:56:04 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterCecryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterCecryptWorker()
+    print("Worker engine initialized.")
